8e44ca51ff8c5e2a3fa125201a8cf177545f094c
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-06-01 10:13:28 -0300

    estudo casting

---
+++ src/main/java/br/com/izaacpinheiro/javastudies/Casting.java
@@ -0,0 +1,16 @@
+package br.com.izaacpinheiro.javastudies;
+
+public class Casting {
+    public static void main(String[] args) {
+        // Converter tipos primitivos
+        // (casting)
+
+        double dd = 10.10;
+        short ss = 32;
+
+        float x = (float) dd / ss; // casting explicito para um float
+
+        int idade = (int) 100000000000000000L; // long -> int
+        System.out.print(idade); // há perda de dados para a conversão
+    }
+}
